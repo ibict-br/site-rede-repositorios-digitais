e1ff7f12fd6882a1b59092fbe0d82471b886a4b2
Marcos Antônio Vieira Faria <marcosfaria@IN.IBICT.BR>
2022-07-05 12:13:13 -0300

    Atualizando novas páginas2

---
+++ page-blog.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+<div id="primary">
+    <div id="main">
+        <div class="container">
+            <?php 
+                while( have_posts() ):
+                    the_post(); ?>
+                    <article id="post-<?php the_ID();?>" <?php post_class(); ?> class="mb-5">
+        <h2 class="titDoc2"><?php the_title(); ?></h2>
+        <div class="meta-info">
+            <p>Categoria: <?php the_category( ' ' ); ?> </p>
+        </div>
+        <div class="meta-text">
+            <p><?php the_content(); ?></p>
+        </div>
+    </article>
+                <?php
+                endwhile;    
+            ?>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
