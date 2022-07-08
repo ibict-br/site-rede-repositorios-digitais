bec6f1c04f203813dde7adccfb71349346e53bc9
Marcos Antônio Vieira Faria <marcosfaria@IN.IBICT.BR>
2022-07-08 12:02:30 -0300

    Página de pesquisa3

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php get_header(); ?>
 <!--Template Banner-->
-<img class="carousel-altura mb-5 img-fluid" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Banner Portal da Rede Brasileira de Repositórios Digitais"/>
+<img class="carousel-altura mb-5 img-fluid" src="<?php header_image(); ?>"
+    height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>"
+    alt="Banner Portal da Rede Brasileira de Repositórios Digitais" />
 <!--Fim Template Banner-->
 <!--Blog-->
 <div class="container mb-5">
     <?php
-      if( have_posts() ): while( have_posts() ) : the_post(); ?>
+      if( have_posts() ): 
+      while( have_posts() ) : the_post(); ?>
     <article class="mb-5">
         <h2 class="titDoc2"><?php the_title(); ?></h2>
         <div class="meta-info">
@@ -19,7 +22,14 @@
         <hr>
     </p>
     <?php
-        endwhile; else: ?>
+        endwhile; 
+        ?>
+            <div class="redes-paginacao">
+                <div class="novo"><?php previous_posts_link( "Novas postagens >>" ); ?></div>
+                <div class="antigo"><?php next_posts_link( "<< Postagens antigas" ); ?></div>
+            </div>
+        <?php 
+        else: ?>
     <p>Documentos não encontrados.</p>
     <?php endif; ?>
 </div>
