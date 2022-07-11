8f00eddd51749727f8082f5a47eafbf7bff965b5
Marcos Antônio Vieira Faria <marcosfaria@IN.IBICT.BR>
2022-07-11 11:23:18 -0300

    Excerpt-3

--- functions.php
+++ functions.php
@@ -52,6 +52,6 @@ add_action( 'after_setup_theme', 'redes_config', 0 );
 
 // Controlando Excerpt
 function my_excerpt_length($length){
-    return 20;
+    return 80;
     }
     add_filter('excerpt_length', 'my_excerpt_length');
