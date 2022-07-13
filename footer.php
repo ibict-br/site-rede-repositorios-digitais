0630faee5e97a2bc906e103807b0d025e86f653e
Marcos Antônio Vieira Faria <marcosfaria@IN.IBICT.BR>
2022-07-13 15:47:43 -0300

    Copyright

--- footer.php
+++ footer.php
@@ -1,21 +1,22 @@
 <!-- Footer -->
 <footer class="bg-dark text-center text-white">
-  <!-- Grid container -->
-  <div class="container-fluid">
-    <!-- Section: Social media -->
-    <section>
-      <!-- Facebook -->
-      <!-- <a class="btn btn-outline-light" href="#!" role="button"><i class="fab fa-facebook-f"></i></a> -->
-    </section>
-    <!-- Section: Social media -->
-  </div>
-  <!-- Fim Grid container -->
-  <!-- Copyright -->
-  <div class="text-center p-5 bgCopyright">
-    © 2022 Copyright:
-    <a class="text-white" href="#!">Rede Brasileira de Repositórios Digitais</a>
-  </div>
-  <!-- Copyright Fim -->
+    <!-- Grid container -->
+    <div class="container-fluid">
+        <!-- Section: Social media -->
+        <section>
+            <!-- Facebook -->
+            <!-- <a class="btn btn-outline-light" href="#!" role="button"><i class="fab fa-facebook-f"></i></a> -->
+        </section>
+        <!-- Section: Social media -->
+    </div>
+    <!-- Fim Grid container -->
+    <!-- Copyright -->
+    <div class="text-center p-5 bgCopyright">
+
+        <p class="text-white">&copy; <?php echo date('Y'); ?> Rede Brasileira de Repositórios Digitais.</p>
+
+    </div>
+    <!-- Copyright Fim -->
 </footer>
 <!-- Fim Footer -->
 
