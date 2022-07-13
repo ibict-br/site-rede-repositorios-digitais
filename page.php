5329ce7289a47eb2a9495c9ea2266d6bb0c7bdcd
Marcos Antônio Vieira Faria <marcosfaria@IN.IBICT.BR>
2022-07-13 16:44:06 -0300

    Modelo Padrão-2

--- page.php
+++ page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <!--Jumbotron-->
 <div class="jumbotron jumbotron-fluid jumbotron-image">
-    <div class="container py-3 pl-5 corDocBannerNordeste">
+    <div class="container py-3 pl-5 corDocBannerPadrao">
         <h1 class="titDoc">Material de Capacitação
         </h1>
     </div>
