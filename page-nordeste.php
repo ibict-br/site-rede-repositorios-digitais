9b4d39c0214f7263dcd7c8c2cf08d750d7e09af8
Marcos Antônio Vieira Faria <marcosfaria@IN.IBICT.BR>
2022-07-13 14:31:39 -0300

    Título

--- page-nordeste.php
+++ page-nordeste.php
@@ -7,7 +7,7 @@ Template Name: Rede Nordeste
 <!--Jumbotron-->
 <div class="jumbotron jumbotron-fluid jumbotron-image">
     <div class="container py-3 pl-5 corDocBannerNordeste">
-        <h1 class="titDoc">Rede Centro-Oeste de Repositórios Digitais
+        <h1 class="titDoc">Rede Nordeste de Repositórios Digitais
         </h1>
     </div>
 </div>
