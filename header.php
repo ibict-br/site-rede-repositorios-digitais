5ec6d29647e77658378ef228a57509f60453c8ad
Marcos Antônio Vieira Faria <marcosfaria@IN.IBICT.BR>
2022-07-13 16:02:44 -0300

    Título site

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Site Redes de Repositórios Digitais</title>
+    <title>Site Rede Brasileira de Repositórios Digitais</title>
     <?php wp_head(); ?>
 </head>
 
