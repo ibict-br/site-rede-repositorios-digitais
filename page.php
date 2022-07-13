604095d3138b8c6d20c497ec1715cb485c9685ce
Marcos Antônio Vieira Faria <marcosfaria@IN.IBICT.BR>
2022-07-13 16:37:00 -0300

    Material de Capacitação

--- page.php
+++ page.php
@@ -1,25 +1,17 @@
 <?php get_header(); ?>
 <!--Jumbotron-->
 <div class="jumbotron jumbotron-fluid jumbotron-image">
-    <div class="container py-3 pl-5 corDocBanner">
-        <h1 class="display-4 titDoc">Sample page
+    <div class="container py-3 pl-5 corDocBannerNordeste">
+        <h1 class="titDoc">Material de Capacitação
         </h1>
-        <p class="lead titDoc">Aqui é a Sample page!</p>
     </div>
 </div>
 <!--Fim Jumbotron-->
-<!--Blog-->
+<!--Documentos-->
 <div class="container mb-5">
     <?php
-      while( have_posts() ) : the_post(); 
-      get_template_part( 'parts/content', 'page' );      
-    ?>
-
-    <p>
-        <hr>
-    </p>
-    <?php
-        endwhile; ?>
+      get_template_part( 'parts/content', 'redes' );
+      ?>
 </div>
-<!--Blog Fim-->
+<!--Documentos Fim-->
 <?php get_footer(); ?>
